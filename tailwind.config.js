399642b96ae40d0d6b0c14129671afc73bd9d391
abhay971 <abhayrana3169@gmail.com>
2024-06-23 12:59:34 +0530

    flaticons and css configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+      
+      fontSize: {
+          'sm': '12px',
+          'base': '14px',
+          'xl': '16px',
+          '2xl': '20px',
+          '3xl': '28px',
+          '4xl': '38px',
+          '5xl': '50px',
+      },
+
+      extend: {
+          fontFamily: {
+            inter: ["'Inter'", "sans-serif"],
+            gelasio: ["'Gelasio'", "serif"]
+          },
+      },
+
   },
   plugins: [],
 }
